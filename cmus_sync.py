e04100935062b40cfa40d1adb7a23f577e262f30
samhal <samhal@users.noreply.github.com>
2015-03-24 23:27:17 +0100

    Added folder functionality and cleaned up

--- cmus_sync.py
+++ cmus_sync.py
@@ -1,88 +1,158 @@
 import subprocess
-import ntpath
+import posixpath
 import time
+import shutil
+import os
 
 def cmus_sync(play_cmus,play_phone,phone):
 	a = set()
 	b = set()
-	rmindex = addindex = 1
+
 	#adds every element of the source playlist
 	with open(play_cmus,"r") as source:
 		for line in source:
 			a.add(line.rstrip('\n'))	
 		if '' in a:
 			a.remove('')
-		source.seek(0)
-	
+
 	#adds every element of the dest playlist
 	with open(play_phone,"r") as dest:
 		for line in dest:	
 			b.add(line.rstrip('\n'))
 		if '' in b:
 			b.remove('')
-		dest.seek(0)
+
 	#these items should be added to dest
 	add = a-b
 	#these items should be removed from dest
 	remove = b-a
-
-	if remove != {}:
-		#% per item	
-		if len(remove) != 0:	
-			pro = (100/float(len(remove)))
-			pr = pro
-		print ("Removing...")
-		#0%
-		print("["+(" "*20)+"]"+" 0%   ",end='\r')
-		#remove files from phone
-		for item in remove:
-			pro5 = int(pr/5)
-			name = ntpath.basename(item)
-			phonepath = phone+"/"+name
-			#1 '#' per 5%
-			print(("["+("#"*pro5)+(" "*(20-pro5))+"]"+" "+str(round(pr,1))+"%   "),end='\r')
-			subprocess.call(["rm","-r",phonepath])
-			pr += pro	
-		else:
-			print(("["+("#"*20)+"]"+" 100%   "))
-			if len(remove) == 1:
-				print ("Done. Removed: %s song"%(len(remove)))
-			else:
-				print ("Done. Removed: %s songs"%(len(remove)))
-
-	if add !={}:
-		if len(add) != 0:	
-			pro = (100/float(len(add)))
-			pr = pro
-		print ("Adding...")
-		print("["+(" "*20)+"]"+" 0%   ",end='\r')
-		#add files to phone
-		for item in add:
-			#add new files to phone
-			pro5 = int(pr/5)
-			name = ntpath.basename(item)
-			print(("["+("#"*pro5)+(" "*(20-pro5))+"]"+" "+str(round(pr,1))+"%   "),end='\r')
-			subprocess.call(["rsync",item,phone])
-			pr += pro	
-		else:
-			print(("["+("#"*20)+"]"+" 100%  "))
-			if len(add) == 1:
-				print("Done. Added: %s new song"%(len(add)))		
-			else:
-				print("Done. Added: %s new songs"%(len(add)))		
-
-
-	subprocess.call(["cp","-r",play_cmus,play_phone])
 	
+	remove_songs(remove,phone)
+	add_songs(add,phone)
 
 
+	shutil.copy(play_cmus,play_phone)
+	
+def remove_songs(remove,phone):
+	
+	# let the user know we're removing
+	print("Removing...")
+	# print the 0% progress, each '#' symbolises 2% 
+	print("["+(" "*50)+"]"+" 0%   ",end='\r')
+	
+	if len(remove) != 0:
+		# ppi = procent per item
+		ppi = (100/float(len(remove)))
+		# prp = progress procent
+		prp = ppi	
+		# remove the files
+		for item in remove:
+			# the path /Artist/Album/Song
+			path_elem = []
+			# number of '#' in progress
+			prog = int(prp/2)
+			# add the directories and song name
+			path = item
+			
+			# get the artist/album/song path
+			for i in range(3):
+				dirs = posixpath.split(path)
+				path_elem.append(dirs[1])
+				path = dirs[0]	
+			
+			artist = phone + "/" + path_elem[2]
+			album = artist + "/" + path_elem[1]
+			song = album + "/" + path_elem[0]
+			
+			os.remove(song)
+			# if the album dir is empty we want to remove it
+			if os.listdir(album) == []:
+				os.rmdir(album)
+				# same goes for the artist dir
+				if os.listdir(artist) == []:
+					os.rmdir(artist)
+
+			# print current progress
+			print(("["+("#"*prog)+(" "*(50-prog))+"]"+" "+str(round(prp))+"%   "),end='\r')
+			prp += ppi
+
+	# print that we're done
+	print(("["+("#"*50)+"]"+" 100%   "))
+	if len(remove) == 1:
+		print ("Done. Removed: %s song\n"%(len(remove)))
+	else:
+		print ("Done. Removed: %s songs\n"%(len(remove)))
+
+
+
+def add_songs(add, phone):
+
+	# let the user know we're removing
+	print("Adding...")
+	# print the 0% progress, each '#' symbolises 2% 
+	print("["+(" "*50)+"]"+" 0%   ",end='\r')
+	
+	if len(add) != 0:
+		# ppi = procent per item
+		ppi = (100/float(len(add)))
+		# prp = progress procent
+		prp = ppi	
+		# remove the files
+		for item in add:
+			# the path /Artist/Album/Song
+			path_elem = []
+			# number of '#' in progress
+			prog = int(prp/2)
+			# add the directories and song name
+			path = item
+
+			# get the artist/album/song path
+			for i in range(3):
+				dirs = posixpath.split(path)
+				path_elem.append(dirs[1])
+				path = dirs[0]	
+
+			artist = phone + "/" + path_elem[2]
+			album = artist + "/" + path_elem[1]
+			song = album + "/" + path_elem[0]
+			
+			
+			# check if artist dir exists
+			if posixpath.exists(artist):
+				# checks if album dir exits
+				if posixpath.exists(album):
+					# copy song to album dir
+					shutil.copy(item,song)
+				else:
+					# create album dir
+					os.makedirs(album)
+					# copy song to album dir
+					shutil.copy(item,song)
+			else:
+				# create artist dir
+				os.makedirs(artist)
+				# create album dir
+				os.makedirs(album)
+				# copy song to album dir
+				shutil.copy(item,song)
+
+			# print current progress
+			print(("["+("#"*prog)+(" "*(50-prog))+"]"+" "+str(round(prp))+"%   "),end='\r')
+			prp += ppi
+
+	# print that we're done
+	print(("["+("#"*50)+"]"+" 100%   "))
+	if len(add) == 1:
+		print ("Done. Added: %s song\n"%(len(add)))
+	else:
+		print ("Done. Added: %s songs\n"%(len(add)))
 
 if __name__=='__main__':
 	play_cmus = play_phone = phone = ""
 	#start time for program
 	start_time = time.time()	
 	
-	with open("cmus_sync.conf","r") as f:
+	with open("/home/sam/.cmus_sync","r") as f:
 		#checks specific files of the config for paths.
 		for i,line in enumerate(f):
 			if i == 1:
@@ -93,7 +163,12 @@ if __name__=='__main__':
 
 			if i == 7:
 				phone = line.rstrip('\n')
-
+	
+	# prints the paths from the config file
+	print("\nMusic destination: %s"%(phone))
+	print("Source playlist: %s"%(play_cmus))
+	print("Phone playlist: %s\n"%(play_phone))
+	
 	cmus_sync(play_cmus, play_phone, phone)
 	#end time for program
 	print("--- %s seconds ---" % (round((time.time() - start_time),3)))
